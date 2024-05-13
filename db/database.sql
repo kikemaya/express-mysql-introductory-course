70d580177ef11fd34019184a69105509da00b843
kikemaya <enrique.mg.cd@gmail.com>
2024-05-13 13:42:15 -0600

    Adding employees post logic

--- db/database.sql
+++ db/database.sql
@@ -14,3 +14,16 @@ DESCRIBE employee;
 
 SELECT
     'Pong' AS result;
+
+INSERT INTO
+    employee
+VALUES
+    (1, "Joe", 1000),
+    (2, "Henry", 2000),
+    (3, "Sam", 2500),
+    (4, "Max", 1500);
+
+SELECT
+    *
+FROM
+    employee;
--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -1,4 +1,17 @@
+import { pool } from "./../db.js";
+
 export const getEmployees = (req, res) => res.send("Getting employees...");
-export const createEmployee = (req, res) => res.send("Creating employee...");
+
+export const createEmployee = async (req, res) => {
+  const { name, salary } = req.body;
+
+  const [rows] = await pool.query(
+    "INSERT INTO employee (name, salary) VALUES (?, ?)",
+    [name, salary]
+  );
+
+  res.status(201).send({ id: rows.insertId, name, salary });
+};
+
 export const updateEmployee = (req, res) => res.send("Updating employee...");
 export const deleteEmployee = (req, res) => res.send("Deleting employee...");
